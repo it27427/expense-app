a58bc9b18c567317918940e53b4579c34429e828
it2427 <istiak.tushar.qtec@gmail.com>
2025-12-20 19:40:58 +0600

    app controller updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
-@Controller()
+@Controller('/report/:reportType')
 export class AppController {
   @Get()
-  getAllIncomeReports() {
+  getAllReports() {
     return [];
+  }
+
+  @Get(':id')
+  getReportById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Deleted';
   }
 }
