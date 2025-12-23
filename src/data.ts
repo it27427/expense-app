7cef501c583b15d122a56ff4221f0ea64171225a
it2427 <istiak.tushar.qtec@gmail.com>
2025-12-23 23:23:18 +0600

    ReportType

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,19 @@
-const data = {
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    createdAt: Date;
+    updatedAt: Date;
+    type: 'income' | 'expense';
+  }[];
+}
+
+enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+export const data: Data = {
   report: [],
 };
